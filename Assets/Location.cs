38e8c764f51ae59c3429eaa3bc6b063d7c620e69
Mika Hahimoto <recruithd_106@r.recruit.co.jp>
2016-09-16 10:50:37 +0900

    Location.csの追加

---
+++ Assets/Location.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Location : MonoBehaviour {
+
+   public string next_location_name;
+   private string type = "location";
+
+　　// Use this for initialization
+   void Start () {
+		
+   }
+	
+   // Update is called once per frame
+   void Update () {
+
+   }
+
+   public string getType () {
+      return type;
+   }
+}
